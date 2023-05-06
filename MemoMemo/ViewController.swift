563e7ae145a54283f8ab104be04d10c9e63faa11
Jun Hyeok, Kim <fixme1537@gmail.com>
2023-05-06 17:56:40 +0900

    Initial Commit

---
+++ MemoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoMemo
+//
+//  Created by Jun Hyeok Kim on 2023/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
